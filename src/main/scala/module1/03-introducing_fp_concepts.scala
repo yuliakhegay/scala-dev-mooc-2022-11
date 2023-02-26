7d39bb6e36cf9a9b8eb9c9a1d29ea89931dec3c5
Юлия Хегай <daedne@Ulias-MacBook-Air.local>
2023-02-26 22:15:21 +0300

    hw1 methods for Option and List

--- src/main/scala/module1/03-introducing_fp_concepts.scala
+++ src/main/scala/module1/03-introducing_fp_concepts.scala
@@ -227,12 +227,40 @@ object hof{
       case Option.None => true
     }
 
+
     def map[B](f: T => B): Option[B] = flatMap(v => Option(f(v)))
+
 
     def flatMap[B](f: T => Option[B]): Option[B] = this match {
       case Option.Some(v) => f(v)
       case Option.None => Option.None
     }
+
+
+    /**
+     *
+     * Реализовать метод printIfAny, который будет печатать значение, если оно есть
+     */
+    def printIfAny: Unit = this match {
+      case Option.Some(v) => println(v)
+      case Option.None => println("Option is empty")
+    }
+
+
+    /**
+     *
+     * Реализовать метод zip, который будет создавать Option от пары значений из 2-х Option
+     */
+    def zip[A](opt: Option[A]): Option[(T, A)] =
+      this.flatMap(a => opt.map(b => (a, b)))
+
+
+    /**
+     *
+     * Реализовать метод filter, который будет возвращать не пустой Option
+     * в случае если исходный не пуст и предикат от значения = true
+     */
+
 
   }
 
@@ -244,30 +272,13 @@ object hof{
     def apply[T](v: T): Option[T] = Some(v)
   }
 
-
-
-
-
-
-  /**
-   *
-   * Реализовать метод printIfAny, который будет печатать значение, если оно есть
-   */
-
-
-  /**
-   *
-   * Реализовать метод zip, который будет создавать Option от пары значений из 2-х Option
-   */
-
-
-  /**
-   *
-   * Реализовать метод filter, который будет возвращать не пустой Option
-   * в случае если исходный не пуст и предикат от значения = true
-   */
+  val opt1: Option[Int] = Option.Some(5)
+  val opt2: Option[Int] = Option.Some(8)
+  opt1.zip(opt2)
 
  }
+
+
 
  object list {
    /**
@@ -275,72 +286,121 @@ object hof{
     * Реализовать односвязанный иммутабельный список List
     * Список имеет два случая:
     * Nil - пустой список
-    * Cons - непустой, содердит первый элемент (голову) и хвост (оставшийся список)
+    * Cons - непустой, содержит первый элемент (голову) и хвост (оставшийся список)
     */
 
    trait List[+T] {
+
+     /**
+      * Метод cons, добавляет элемент в голову списка, для этого метода можно воспользоваться названием `::`
+      *
+      */
      def ::[TT >: T](elem: TT): List[TT] = ???
+
+
+     /**
+      *
+      * Реализовать метод map для списка который будет применять некую ф-цию к элементам данного списка
+      */
+     def map[B](f: T => B): List[B] = this match {
+       case List.::(head, tail) =>
+         val newHead = f(head)
+         val newTail = for (elem <- tail) yield f(elem)
+         newHead :: newTail
+       case List.Nil => List.Nil
+       case _ => ???
+     }
+
+
+     def flatMap[B](f: T => List[B]): List[B] = this match {
+       case List.::(head, tail) => ???
+       case List.Nil => List.Nil
+       case _ => ???
+     }
+
+
+     /**
+      * Метод mkString возвращает строковое представление списка, с учетом переданного разделителя
+      *
+      */
+     def mkString(sep: String): String = {
+       val listAsString = new StringBuilder()
+       this.map(listAsString + sep + _)
+       listAsString.toString
+     }
+
+
+     /**
+      *
+      * Реализовать метод reverse который позволит заменить порядок элементов в списке на противоположный
+      */
+     def reverse: List[T] = ???
+
+
+     /**
+      *
+      * Реализовать метод filter для списка который будет фильтровать список по некому условию
+      */
+     def filter(f: T => Boolean): List[T] = {
+       def loop(inputList: List[T], outputList: List[T] = List.Nil): List[T] =
+         inputList match {
+           case List.Nil => outputList
+           case List.::(head, tail) => loop(tail, f(head) :: outputList)
+       }
+       loop(this).reverse
+     }
+
+
+     /**
+      *
+      * Написать функцию incList котрая будет принимать список Int и возвращать список,
+      * где каждый элемент будет увеличен на 1
+      */
+     def incList: List[Int] = this.map(num => num + 1)
+
+
+     /**
+      *
+      * Написать функцию shoutString котрая будет принимать список String и возвращать список,
+      * где к каждому элементу будет добавлен префикс в виде '!'
+      */
+     def shoutString: List[String] = this.map("!" + _)
+
+
+//     Нужен ли здесь паттерн матчинг? Кажется нет
+     def shoutString2: List[String] = this match {
+       case List.::(head, tail) =>
+         val shoutHead = "!" + head
+         val shoutTail = tail.map("!" + _)
+         shoutHead :: shoutTail
+       case List.Nil => List.Nil
+       case _ => ???
+     }
    }
+
+   def incList(lst: List[Int]): List[Int] = lst.map(_ + 1)
+
+   def shoutString(lst: List[String]): List[String] = lst.map("!" + _)
+
 
    object List {
      case class ::[A](head: A, tail: List[A]) extends List[A]
      case object Nil extends List[Nothing]
 
+     /**
+      * Конструктор, позволяющий создать список из N - го числа аргументов
+      * Для этого можно воспользоваться *
+      *
+      * Например вот этот метод принимает некую последовательность аргументов с типом Int и выводит их на печать
+      * def printArgs(args: Int*) = args.foreach(println(_))
+      */
      def apply[A](v: A*): List[A] =
        if(v.isEmpty) Nil else ::(v.head, apply(v.tail:_*))
    }
 
-   List(1, 2, 3)
-
-
-
-
-
-   /**
-     * Метод cons, добавляет элемент в голову списка, для этого метода можно воспользоваться названием `::`
-     *
-     */
-
-    /**
-      * Метод mkString возвращает строковое представление списка, с учетом переданного разделителя
-      *
-      */
-
-    /**
-      * Конструктор, позволяющий создать список из N - го числа аргументов
-      * Для этого можно воспользоваться *
-      * 
-      * Например вот этот метод принимает некую последовательность аргументов с типом Int и выводит их на печать
-      * def printArgs(args: Int*) = args.foreach(println(_))
-      */
-
-    /**
-      *
-      * Реализовать метод reverse который позволит заменить порядок элементов в списке на противоположный
-      */
-
-    /**
-      *
-      * Реализовать метод map для списка который будет применять некую ф-цию к элементам данного списка
-      */
-
-
-    /**
-      *
-      * Реализовать метод filter для списка который будет фильтровать список по некому условию
-      */
-
-    /**
-      *
-      * Написать функцию incList котрая будет принимать список Int и возвращать список,
-      * где каждый элемент будет увеличен на 1
-      */
-
-
-    /**
-      *
-      * Написать функцию shoutString котрая будет принимать список String и возвращать список,
-      * где к каждому элементу будет добавлен префикс в виде '!'
-      */
+   val l1 = List(1, 2, 3)
+   val l2 = List("one", "two", "three")
+   l1.mkString("!") // 1!2!3
+   l2.mkString(":") //one:two:three
 
  }
